c91f29fd3426110c290e76157cf05b1c86975a3d
iagoMachado96 <202204384833@alunos.estacio.br>
2022-09-07 09:39:35 -0300

    Aula de manipulação de arquivos usando as funções touch(), rename(), file_exixts(), copy() e unlink().

---
+++ files/files.php
@@ -0,0 +1,27 @@
+<?php
+
+/* A função touch() cria um arquivo. A extensão nesse caso não é obrigatória, mas pode ser passada */
+//var_dump(touch('test.txt'));
+
+
+
+/* A função rename() renomeia um arquivo */
+//var_dump(rename('test.txt', 'test2.txt'));
+
+
+
+/* A função file_exists() verifica se um arquivo existe */
+//var_dump(file_exists('test.txt'));
+//echo '<hr>';
+//var_dump(file_exists('test2.txt'));
+
+
+
+/* A função copy() serve para copiar um arquivo. O primeiro parâmetro é o nome do arquivo a ser copiado e o segundo é o caminho onde o arquivo será copiado ou o novo nome que o arquivo terá depois de copiado */
+//var_dump(copy('test2.txt', 'test.txt'));
+
+
+
+/* A função unlink() serve para remover um arquivo */
+//var_dump(unlink('test2.txt'));
+?>
